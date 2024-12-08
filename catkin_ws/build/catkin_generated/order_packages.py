e70064d6a9887eae00f65f194197080ba1769625
Rodrigo <rdur5926@gmail.com>
2024-12-08 13:00:05 +0100

    Cmakelist fix2

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/rdur5926/RP_Nazarie_Horia_Duran_Rodrigo/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/rdur5926/RP_Nazarie_Horia_Duran_Rodrigo/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/rdur5926/RP_Nazarie_Horia_Duran_Rodrigo/catkin_ws/devel;/opt/ros/noetic' != '' else []
